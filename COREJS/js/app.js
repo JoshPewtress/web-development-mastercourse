47514623cd8d655bb26ffc85256d042cf90eb9e7
Josh Pewtress <61856857+JoshPewtress@users.noreply.github.com>
2025-12-02 17:31:46 -0700

    [Learn] JavaScript Variables

--- COREJS/js/app.js
+++ COREJS/js/app.js
@@ -1,2 +1,21 @@
-console.log('Hello World, From another file');
-console.log('This is Josh Pewtress');
+let testString = 'this is a test';
+const testNumber = 45;
+const testBoolean = true;
+
+testString = 'this is a better test';
+
+console.log(testString);
+
+// hoisting
+
+/*
+i = 10;
+i = 5;
+console.log(i);
+var i = 2;
+*/
+
+j = 10;
+j = 5;
+console.log(j);
+let j = 2;
