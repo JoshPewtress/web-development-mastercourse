8c4ed7429137d587e5afa6b4e586f2d2d5acf128
Josh Pewtress <61856857+JoshPewtress@users.noreply.github.com>
2025-12-04 12:03:30 -0700

    [Practice] If and Switch statements, Logical and Equality Operators

--- COREHTML/Practice/js/app.js
+++ COREHTML/Practice/js/app.js
@@ -1,22 +1,29 @@
-const testArray = ['First', 'Second', 'Third'];
+let temperature = 68;
 
-console.log(testArray);
+const hot = 71;
+const cold = 65;
 
-testArray.push('Fourth');
-testArray.push('Fifth');
+if (temperature < hot && temperature > cold) {
+    console.log('It is a comfortable temperature');
+} else if (temperature >= hot) {
+    console.log('It is too hot');
+} else if (temperature <= cold) {
+    console.log('It is too cold');
+} else {
+    console.log('Unknown Temperature');
+}
 
-console.log(testArray);
-
-const startsWithF = testArray.filter(function(_) {
-    return _.startsWith('F');
-});
-
-console.log(startsWithF);
-
-const poped = testArray.pop();
-
-const newStartsWithF = testArray.filter(function(_) {
-    return _.startsWith('F');
-});
-
-console.log(poped == 'Fifth');
+switch (true) {
+    case temperature >= hot:
+        console.log('It is too hot, switch.');
+        break;
+    case temperature <= cold:
+        console.log('It is too cold, switch');
+        break;
+    case temperature < hot && temperature > cold:
+        console.log('It is a good temperature, switch');
+        break;
+    default:
+        console.log('Unknown temperature, switch');
+        break;
+}
