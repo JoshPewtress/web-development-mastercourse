a3f2537064376b85de5467edb5f794f129be114b
Josh Pewtress <61856857+JoshPewtress@users.noreply.github.com>
2025-12-09 15:30:42 -0700

    [Practice] Strict Mode

--- COREHTML/Practice/js/app.js
+++ COREHTML/Practice/js/app.js
@@ -1,32 +1,11 @@
-var a = 1;
-let b = 11;
-const c = 21;
+'use strict';
 
-/*
-if (true) {
-    var a = 2; // overwrote global `a`
-    let b = 12;
-    const c = 22;
+const me = {
+    firstName: 'Josh',
+    lastName: 'Pewtress',
+    fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
 }
-*/
 
-/*
-function testing() {
-    var a = 2;
-    let b = 12;
-    const c = 22;
-}
-testing();
-*/
-
-/*
-for (let i = 0; i < 1; i++) {
-    var a = 2; // overwrote global `a`
-    let b = 12;
-    const c = 22;
-}
-*/
-
-console.log(a);
-console.log(b);
-console.log(c);
+console.log(me.fullName());
