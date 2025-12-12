8a2900a91bed4b651dc38e288e007d3b61923799
Josh Pewtress <61856857+JoshPewtress@users.noreply.github.com>
2025-12-11 20:15:58 -0700

    [Practice] Traversing Elements

--- COREHTML/Practice/js/app.js
+++ COREHTML/Practice/js/app.js
@@ -1,5 +1,17 @@
 'use strict';
 
-// const topicBlocks = document.querySelectorAll('li').forEach(el => {
-//     el.style.backgroundColor = 'rgba(143, 191, 237, 0.2)';
-// });
+// const sections = document.querySelectorAll('.topics-section');
+// const sectionsArray = Array.from(sections);
+
+// sectionsArray.forEach(el => console.log(el));
+
+const navigation = document.querySelector('nav');
+let navItem = navigation.querySelector('li');
+
+while (navItem !== null) {
+    console.log(navItem);
+    navItem = navItem.nextElementSibling;
+}
+
+
+
